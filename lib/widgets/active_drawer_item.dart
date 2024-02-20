e92b1dc7eda36ee713ef50cca0074f751838e4a3
osama_hassan20 <osamahassan210320@gmail.com>
2024-02-21 01:11:59 +0200

    done

---
+++ lib/widgets/active_drawer_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../models/drawer_item_model.dart';
+import '../utils/app_styles.dart';
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: FittedBox(
+        alignment: AlignmentDirectional.centerStart,
+        fit: BoxFit.scaleDown,
+        child: Text(
+          drawerItemModel.title,
+          style: AppStyles.styleMedium16(context),
+        ),
+      ),
+    );
+  }
+}
